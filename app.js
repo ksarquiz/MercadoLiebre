a09aab5f484a2531829fad45cdbbe17c39f33f41
kristina Sarquiz <sarquizkristina@gmail.com>
2022-02-01 20:12:04 -0300

    cambio app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
-const path = require ("path");
+const path = require("path");
 
 //const publicPath= path.resolve(__dirname,"./public");
-app.use("/static",express.static(__dirname + "/public"));
+app.use("/static", express.static(__dirname + "/public"));
 
 
-app.listen(3030,()=>{console.log("servidor corriendo")});
-app.get("/",function(req,res){
-    res.sendFile(path.resolve(__dirname,"./views/home.html"));
+app.listen( process.env.PORT || 3030, () => { console.log("servidor corriendo") });
+app.get("/", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"));
 });
